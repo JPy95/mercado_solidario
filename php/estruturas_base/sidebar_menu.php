acf308d310140062f98f011a51b5f865a8453322
Tubias35 <35923956+Tubias35@users.noreply.github.com>
2019-01-08 22:04:51 -0200

    Finalização do menu sidebar da pagina de perfil de usuarios

--- php/estruturas_base/sidebar_menu.php
+++ php/estruturas_base/sidebar_menu.php
@@ -1,25 +1,20 @@
-<div id="sidebar_perfil">
-    <div class="sidebar_titulo">
-        <img src="img\icones\menu.png" class="sidebar_img"><span class="sidebar_perfil">Meu Perfil</span>
+<div id="sidebar_menu">
+    <div class="title_sidebar">
+        <div>
+            <img class="img_side" src="img\icones\avatar_side.png">
+        </div>
+        <div>
+            <h4 class="texte_title_side">Nome do Usuário</h4>
+        </div>
     </div>
-    <div class="sidebar_item_group">
-        <div class="sidebar_menu">
-            <img src="img\icones\dash.png" class="sidebar_img">
-            <button class="btn_sidebar">
-                <span class="">Dashboard</span>
-            </button>
-        </div>
-        <div class="sidebar_menu">
-            <img src="img\icones\compras.png" class="sidebar_img">
-            <button class="btn_sidebar">
-                <span class="">Compras</span>
-            </button>
-        </div>
-        <div class="sidebar_menu">
-            <img src="img\icones\vendas.png" class="sidebar_img">
-            <button class="btn_sidebar">
-                <span class="">Vendas</span>
-            </button>
+    <div class="dropdown show">
+        <a class="btn_dropdown dropdown-toggle" href="#" role="button" id="sidebar_compras" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="sidebar_compras">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
         </div>
     </div>
 </div>
