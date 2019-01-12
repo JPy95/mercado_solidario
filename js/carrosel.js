93dfcd4ac90c6ff7ea76cc478a5f6ed459406ccd
Tubias35 <35923956+Tubias35@users.noreply.github.com>
2019-01-12 01:10:35 -0200

    elaborando o carousel

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,12 +1,22 @@
 function mover(direcao) {
     var elemento = document.getElementById('elementos');
-    var botoes = document.querySelectorAll('a.carrosel_a');
+    var botoes = document.querySelectorAll('button.btn_carousel');
     var voltar = botoes[0];
     var avancar = botoes[1];
 
-    elemento.scrollLeft += 20 * direcao;
+    elemento.scrollLeft += 200 * direcao;
     var inicio = elemento.scrollLeft == 0;
     var fim = elemento.clientWidth + elemento.scrollLeft - elemento.scrollWidth;
     voltar.style.opacity = inicio ? 0 : 1;
     avancar.style.opacity = fim ? 1 : 0;
+}
+
+function valida_tamanho(){
+    var elemento = document.getElementById('elementos');
+    var botoes = document.querySelectorAll('button.btn_carousel');
+    var voltar = botoes[0];
+    var avancar = botoes[1];
+    if(elemento.clientWidth==elemento.scrollWidth){
+        avancar.style.opacity = 0;
+    } 
 }
--- src_inst.php
+++ src_inst.php
@@ -5,7 +5,7 @@
         <script type="text/javascript" src="js\carrosel.js"></script>
         <link rel="stylesheet" type="text/css" href="css\carrosel.css">
     </head>
-    <body>
+    <body onload="valida_tamanho()">
         <!--NAV -->
         <?php include_once('php\estruturas_base\nav_principal.php') ?>
          <div id="busca_inst">
@@ -47,15 +47,80 @@
             <div class="border_bottom_src_inst">
                 <h4 class="style_subtitle_slide">Causa 1</h4>
             </div>
-            <div class="desc_proj_inst">
+            <div  class="card_inst_group">
+                <!--Voltar-->
+                <button class="btn_carousel btn_previous" onClick="mover(-1);" style="opacity: 0;"></button>
+                <!--Cards-->
                 <ul id="elementos" class="carrosel_ul">
-                    <li class="carrosel_li"><?php include_once('php\estruturas_base\card_inst.php')?></li>
-                    <li class="carrosel_li"><?php include_once('php\estruturas_base\card_inst.php')?></li>
-                    <li class="carrosel_li"><?php include_once('php\estruturas_base\card_inst.php')?></li>
-                    <li class="carrosel_li"><?php include_once('php\estruturas_base\card_inst.php')?></li>
+                    <li class="carrosel_li">
+                        <div class="card_proj_inst">
+                            <div class="borda_img_card">
+                                <img src="img\img_prod\baleia.png" class="img_card_aprest_perfil_inst">
+                            </div>
+                            <div class="desc_projt_card_prfil_inst">
+                                <h5>Nome Fantasia1</h5>
+                            </div>
+                            <div >
+                                <button class="btn_cards">Saiba Mais</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="carrosel_li">
+                        <div class="card_proj_inst">
+                            <div class="borda_img_card">
+                                <img src="img\img_prod\baleia.png" class="img_card_aprest_perfil_inst">
+                            </div>
+                            <div class="desc_projt_card_prfil_inst">
+                                <h5>Nome Fantasia2</h5>
+                            </div>
+                            <div >
+                                <button class="btn_cards">Saiba Mais</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="carrosel_li">
+                        <div class="card_proj_inst">
+                            <div class="borda_img_card">
+                                <img src="img\img_prod\baleia.png" class="img_card_aprest_perfil_inst">
+                            </div>
+                            <div class="desc_projt_card_prfil_inst">
+                                <h5>Nome Fantasia3</h5>
+                            </div>
+                            <div >
+                                <button class="btn_cards">Saiba Mais</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="carrosel_li">
+                        <div class="card_proj_inst">
+                            <div class="borda_img_card">
+                                <img src="img\img_prod\baleia.png" class="img_card_aprest_perfil_inst">
+                            </div>
+                            <div class="desc_projt_card_prfil_inst">
+                                <h5>Nome Fantasia4</h5>
+                            </div>
+                            <div >
+                                <button class="btn_cards">Saiba Mais</button>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="carrosel_li">
+                        <div class="card_proj_inst">
+                            <div class="borda_img_card">
+                                <img src="img\img_prod\baleia.png" class="img_card_aprest_perfil_inst">
+                            </div>
+                            <div class="desc_projt_card_prfil_inst">
+                                <h5>Nome Fantasia5</h5>
+                            </div>
+                            <div >
+                                <button class="btn_cards">Saiba Mais</button>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
-                <a class="carrosel_a" href="#elementos" onClick="mover(-1);" style="opacity: 0;">&lt; Voltar </a>
-                <a class="carrosel_a" href="#elementos" onClick="mover(1);">Avançar &gt;</a>
+                <!--Fim Cards-->
+                <!--Avançar-->
+                <button class="btn_carousel btn_next" onClick="mover(1);"></button>
             </div>
         </div>
         <!--Rodapé-->
