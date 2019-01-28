6e47eb326368bc3ac1622a363eac81d306d5d1b8
RicarteElias <ricarte.elias@outlook.com>
2019-01-28 21:29:13 -0200

    Classe conexao inserida(Copia do exemplo de web)

---
+++ php/Conexao/Conexao.php
@@ -0,0 +1,58 @@
+<?php
+
+class Conexao
+{
+ 
+   /*
+     * Declaração dos atributos da classe de conexão
+     */
+
+    private $host; // Endereço do servidor do banco de dados
+    private $bd; // Banco de dados utilizado na conexão
+    private $usuario; // Usuário do banco de dados que possua acesso ao schema
+    private $senha; // Senha do usuário
+    private $sql; // Consulta a ser executada
+    private $conexao;
+
+    private function configuraDados()
+    {
+        $this->bd = 'bd';
+        $this->host = 'localhost';
+        $this->usuario = 'root';
+        $this->senha = '';
+    }
+
+    function __construct()
+    {
+        try {	
+			/*
+             * Método que conecta ao banco de dados passando
+             * os valores necessários para que a conexão ocorra
+             */
+            $this->configuraDados();
+            $this->conectar();
+        } catch (PDOException $e) {
+            # call the get_error function
+            $this->get_error($e);
+        }
+    }
+
+    public function __sleep()
+    {
+        return array('bd', 'host', 'usuario', 'senha');
+    }
+
+    public function __wakeup()
+    {
+        $this->conectar();
+    }
+
+    function conectar()
+    {
+        $query = "mysql:host=" . $this->host . ";dbname=" . $this->bd;
+        $this->conexao = new PDO($query, $this->usuario, $this->senha);
+        return $this->conexao;
+    }
+}
+
+?>
