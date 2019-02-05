7e9a07d6a7187e1d8c13f241d4352bad2f965172
RicarteElias <ricarte.elias@outlook.com>
2019-02-04 22:44:53 -0200

    Continuação do CRUD

--- php/Entidades/Pessoa.php
+++ php/Entidades/Pessoa.php
@@ -4,18 +4,102 @@ class Usuario
 
 {
     private $data_cadastro;
-    private $cpf;
     private $nome;
+    private $tipo_pessoa;
+    private $cpf_cnpj;
     private $email;
     private $senha;
-    private $endereco;
+   
 
     //construtor
-    public function __construct($email, $senha, $endereco)
+    public function __construct($data_cadastro, $nome, $tipo_pessoa, $cpf_cnpj, $email, $senha)
     {
+        $this->data_cadastro = $data_cadastro;
+        $this->nome = $nome;
+        $this->tipo_pessoa = $tipo_pessoa;
+        $this->cpf_cnpj = $cpf_cnpj;
         $this->email = $email;
         $this->senha = $senha;
-        $this->endereco = $endereco;
+    }
+
+    public function insert($conexao)
+    {
+        $con = $conexao->conectar();
+
+        $query = "INSERT INTO USUARIO(nome, cpf, email, senha) 
+							values(
+                                '" . getdate() . "',
+								'" . $this->getNome() . "',
+								'" . $this->getCpf() . "',
+								'" . $this->getEmail() . "',
+								'" . $this->getSenha() . "'                                
+							)";
+
+        $stmt = $con->prepare($query);
+        return $stmt->execute();
+    }
+
+
+
+
+    /**
+     * Get the value of data_cadastro
+     */
+    public function getData_cadastro()
+    {
+        return $this->data_cadastro;
+    }
+
+    /**
+     * Set the value of data_cadastro
+     *
+     * @return  self
+     */
+    public function setData_cadastro($data_cadastro)
+    {
+        $this->data_cadastro = $data_cadastro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cpf_cnpj
+     */
+    public function getCpf_cnpj()
+    {
+        return $this->cpf_cnpj;
+    }
+
+    /**
+     * Set the value of cpf_cnpj
+     *
+     * @return  self
+     */
+    public function setCpf_cnpj($cpf_cnpj)
+    {
+        $this->cpf_cnpj = $cpf_cnpj;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
     }
 
     /**
@@ -58,24 +142,22 @@ class Usuario
         return $this;
     }
 
-
-
     /**
-     * Get the value of endereco
+     * Get the value of tipo_pessoa
      */
-    public function getEndereco()
+    public function getTipo_pessoa()
     {
-        return $this->endereco;
+        return $this->tipo_pessoa;
     }
 
     /**
-     * Set the value of endereco
+     * Set the value of tipo_pessoa
      *
      * @return  self
      */
-    public function setEndereco($endereco)
+    public function setTipo_pessoa($tipo_pessoa)
     {
-        $this->endereco = $endereco;
+        $this->tipo_pessoa = $tipo_pessoa;
 
         return $this;
     }
