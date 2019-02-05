b592c8d75bce051c9f217986ebc9682043b27e89
Tubias35 <35923956+Tubias35@users.noreply.github.com>
2019-02-05 20:47:25 -0200

    carrinho

--- php/Entidades/Carrinho.php
+++ php/Entidades/Carrinho.php
@@ -7,10 +7,11 @@
 
         public function insert($idUsuario,$conexao){
             $con = $conexao->conectar();
+            $tokenCart = $this->getTokenCart();
 
             foreach($this->getCarrinho() as $cart){
-                $query="INSERT INTO carrinho(tokenCart,nomePdt,vlrPdt,doacao,idVendedor,idUsuario)
-                        VALUES( '" . $this->getTokenCart() . "',
+                $query="INSERT INTO carrinho(tokenCart,vlrPdt,doacao,qtde,idUsuario)
+                        VALUES( '" . $tokenCart . "',
                                 '" . $cart[0] . "',
                                 '" . $cart[1][0] . "',
                                 '" . $cart[1][1] . "',
@@ -20,34 +21,5 @@
                                 )";
             }
         }
-
-        public function getTokenCart() {
-            return $this->tokenCart = rand(1,1000000);
-        }
-
-        public function addPdt($idPdt){
-            //chamar metodo de select de produto, pegar os seguintes dados (nome,valor,doacao,idVendedor);
-            $this->carrinho[] = array("idPdt"=>$idPdr,array('nomePdr'=>$nomePdt,'vlrPdt'=>$vlrPdt,'doacao'=>$doacao,'idVendedor'=>$idVendedor));
-            return $carrinho;
-        } 
-
-        public function getCarrinho() {
-            return $this->carrinho;
-        }
     }
-    
-    $produto1 = array(1,array("airfryer",1000,10,1));
-    $produto2 = array(2,array("Airfryer",1500,15,2));
-    $produto3 = array(3,array("Airfryer",1500,15,2));
-
-    $carrinho[] = $produto1;
-    $carrinho[] = $produto2;
-    $carrinho[] = $produto3;
-
-    var_dump($carrinho);
-
-    foreach($carrinho as $cart){
-        echo "Dados: {$cart[0]}-{$cart[1][1]}\n";
-    }
-
 ?>
---
+++ php/objetos/_carrinho.php
@@ -0,0 +1,27 @@
+<?php
+    
+    
+    
+    public function addCarrinho($idPdt,$vlrPdt,$qtde){
+        $carrinho[] = array($idPdt,array($vlrPdt,$qtde));
+    }
+
+    public function 
+    
+    $produto1 = array(1,array(1000,1));
+    $produto2 = array(2,array(1500,1));
+    $produto3 = array(3,array(1500,2));
+
+    $carrinho[] = $produto1;
+    $carrinho[] = $produto2;
+    $carrinho[] = $produto3;
+
+    var_dump($carrinho);
+
+    foreach($carrinho as $cart){
+        echo "Dados: {$cart[0]}-{$cart[1][1]}\n";
+    }
+    
+
+
+?>
