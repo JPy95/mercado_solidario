7a485eeebfb7039c6acb4f818b1c9f80a96f82bb
RicarteElias <ricarte.elias@outlook.com>
2019-02-06 23:06:26 -0200

    Criação da pagina finalizar compras

---
+++ compra.php
@@ -0,0 +1,14 @@
+<html lang="pt-br">
+    <head>
+        <?php include_once('php\estruturas_base\head.php') ?>
+        <script type="text/javascript" src="js\validacao_login.js"></script>
+    </head>
+    <body>
+        <nav>
+            <?php include_once('php\estruturas_base\nav_principal.php') ?>
+        </nav>
+        
+        <!--Rodapé-->
+        <?php include_once('php\estruturas_base\footer.php') ?>
+  </body>
+</html>
